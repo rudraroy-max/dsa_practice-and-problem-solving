0f7a9a0269641c7d19ef578db369b51dc2da378d
Rudra Roy <rudraroy192@gmail.com>
2026-05-31 20:56:51 +0530

    recursion 3rd problem

--- 14_Recursion_problems.cpp
+++ 14_Recursion_problems.cpp
@@ -17,10 +17,27 @@ int fact(int n){
         return n*fact(n-1);
 }
 
+// power of M using recursion
+int pow(int m , int n){
+    if(n==0)
+        return 1;
+    else
+        return m*pow(m,n-1);
+}
+int pow1(int m,int n){
+    if(n==0)
+        return 1;
+    if(n%2 == 0)
+        return pow(m*m,n/2);
+    else
+        return m*pow(m*m,(n-1)/2);
+}
+
 int main(){
     int n ;
     cin>>n;
     cout<<"sum is "<<sum(n)<<endl;
     cout<<"factorial is "<<fact(n)<<endl;
+    cout<<"power is "<<pow1(2,n)<<endl;
     return 0;
 }
