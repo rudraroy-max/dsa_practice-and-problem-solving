33dcca1baef5c717565503cc30eb5dbacc1a1b8f
Rudra Roy <rudraroy192@gmail.com>
2026-05-31 20:32:46 +0530

    recursion 2nd problem

--- 14_Recursion_problems.cpp
+++ 14_Recursion_problems.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+// sum of N natural numbers
 int sum(int n){
     if(n==0)
         return 0 ;
@@ -8,9 +9,18 @@ int sum(int n){
         return sum(n-1)+n;
 }
 
+// factorial of N
+int fact(int n){
+    if(n==0)
+        return 1;
+    else
+        return n*fact(n-1);
+}
+
 int main(){
     int n ;
     cin>>n;
     cout<<"sum is "<<sum(n)<<endl;
+    cout<<"factorial is "<<fact(n)<<endl;
     return 0;
 }
